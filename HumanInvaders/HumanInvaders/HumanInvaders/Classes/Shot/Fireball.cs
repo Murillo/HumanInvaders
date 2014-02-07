f12bdbb3c76d5874fe6eccc83cd3f87169725de8
Murillo Grübler <msgrubler@gmail.com>
2014-02-06 22:17:46 -0200

    Revert "The files were moved to other folder"
    This reverts commit 12782121d9b58eea260fb5740ca6a91a1a8acdac.

---
+++ HumanInvaders/HumanInvaders/HumanInvaders/Classes/Shot/Fireball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace HumanInvaders.Shot
+{
+    class Fireball
+    {
+        Vector2 position;
+        Texture2D picture;
+        float speed;
+        public Rectangle Retangulo { get; set; }
+
+        public Vector2 Position { get { return position; } }
+
+        public Fireball(Texture2D firePicture, Vector2 startPosition, float updateSpeed)
+        {
+            picture = firePicture;
+            position = startPosition;
+            speed = updateSpeed;
+            Retangulo = new Rectangle(0, 0, 12, 12);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            position.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+
+            batch.Draw(picture, position, Retangulo, Color.White, 0.0f, new Vector2(10.0f, 10.0f), 1.0f, SpriteEffects.None, 1.0f);
+        }
+        
+    }
+}
